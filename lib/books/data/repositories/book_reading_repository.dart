da80c101b306840122421e12e4a889557bbfb2c8
João Marcos Kaminoski de Souza <kamisou.dev@gmail.com>
2023-07-25 17:30:43 -0300

    WIP stub repositório de leituras de livros offline

--- lib/books/data/repositories/book_reading_repository.dart
+++ lib/books/data/repositories/book_reading_repository.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reading/books/domain/models/book_reading.dart';
 import 'package:reading/books/domain/value_objects/pages.dart';
+import 'package:reading/common/infrastructure/datasources/connectivity.dart';
 import 'package:reading/common/infrastructure/datasources/rest_api.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -8,7 +9,9 @@ part 'book_reading_repository.g.dart';
 
 @riverpod
 BookReadingRepository bookReadingRepository(BookReadingRepositoryRef ref) {
-  return OnlineBookReadingRepository(ref);
+  return ref.watch(isConnectedProvider)
+      ? OnlineBookReadingRepository(ref)
+      : OfflineBookReadingRepository(ref);
 }
 
 @riverpod
@@ -43,5 +46,21 @@ class OnlineBookReadingRepository extends BookReadingRepository {
         .get('books/$bookId/readings')
         .then((response) => (response as List<Json>).map(BookReading.fromJson))
         .then((bookReadings) => bookReadings.toList());
+  }
+}
+
+class OfflineBookReadingRepository extends BookReadingRepository {
+  const OfflineBookReadingRepository(super.ref);
+
+  @override
+  Future<void> addReading(int bookId, Pages pages) {
+    // TODO(kamisou): métodos para update offline
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BookReading>> getBookReadings(int bookId) {
+    // TODO(kamisou): buscar leituras relacionadas com bookId
+    throw UnimplementedError();
   }
 }
