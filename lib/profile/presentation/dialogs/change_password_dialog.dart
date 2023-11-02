af4d533392aab8fd9e14caac66d168ae5ab74201
João Marcos Kaminoski de Souza <kamisou.dev@gmail.com>
2023-11-02 16:13:12 -0300

    UserAppBar vai para perfil, diálogo de mudança de senha não fecha antes de sucesso.

--- lib/profile/presentation/dialogs/change_password_dialog.dart
+++ lib/profile/presentation/dialogs/change_password_dialog.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:reading/authentication/domain/value_objects/password.dart';
+import 'package:reading/profile/presentation/controllers/profile_controller.dart';
 import 'package:reading/profile/presentation/hooks/use_password_form_reducer.dart';
+import 'package:reading/shared/exceptions/repository_exception.dart';
+import 'package:reading/shared/exceptions/rest_exception.dart';
+import 'package:reading/shared/presentation/hooks/use_controller_listener.dart';
 import 'package:reading/shared/presentation/widgets/obsfuscated_text_form_field.dart';
 
-class ChangePasswordDialog extends HookWidget {
+class ChangePasswordDialog extends HookConsumerWidget {
   const ChangePasswordDialog({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final formKey = useRef(GlobalKey<FormState>());
     final passwordForm = usePasswordFormReducer();
+
+    useControllerListener(
+      ref,
+      controller: profileControllerProvider,
+      onError: (error) => switch (error) {
+        BadResponseRestException(message: final message) => message,
+        OnlineOnlyOperationException() => 'Você precisa conectar-se à internet',
+        _ => null,
+      },
+      onSuccess: context.pop,
+    );
 
     return Dialog(
       child: Padding(
@@ -85,9 +101,13 @@ class ChangePasswordDialog extends HookWidget {
                 Expanded(
                   child: FilledButton(
                     onPressed: () {
-                      if (formKey.value.currentState!.validate()) {
-                        context.pop(passwordForm.state);
+                      if (!formKey.value.currentState!.validate()) {
+                        return;
                       }
+
+                      ref
+                          .read(profileControllerProvider.notifier)
+                          .savePassword(passwordForm.state);
                     },
                     child: const Text('Salvar'),
                   ),
